7c429935758d8ae6af37f786f6432ee4b2754c0f
Tomer <carollatomer@gmail.com>
2022-11-13 22:38:04 +0200

    remove switch button

--- src/components/registerVideo/index.js
+++ src/components/registerVideo/index.js
@@ -1,7 +1,46 @@
 import {StyledRegisterVideo} from "./registerVideoStyles";
 import {useState} from "react";
 
-function userForm(formProps) {
+export default function RegisterVideo() {
+    const [isModalShow, setModalShow] = useState(false);
+    const registerForm = useForm({
+        initialValues: {title: '', url: ''}
+    });
+
+    return (
+        <StyledRegisterVideo>
+            <button className="add-video" onClick={() => setModalShow(true)}>+</button>
+            {
+                isModalShow && (
+                    <form onSubmit={(event) => {
+                        event.preventDefault();
+                        setModalShow(false);
+                        registerForm.clearForm();
+                    }}>
+                        <div>
+                            <button type="button" className="close-modal" onClick={() => {
+                                setModalShow(false);
+                                registerForm.clearForm();
+                            }}>x</button>
+
+                            <input type="text" placeholder="Video title" value={registerForm.values.title}
+                                   name="title"
+                                   onChange={registerForm.handleChange}/>
+
+                            <input type="text" placeholder="Url" value={registerForm.values.url}
+                                   name="url"
+                                   onChange={registerForm.handleChange}/>
+
+                            <button type="submit">Register</button>
+                        </div>
+                    </form>
+                )
+            }
+        </StyledRegisterVideo>
+    )
+}
+
+function useForm(formProps) {
     const [values, setValues] = useState(formProps.initialValues);
 
     return {
@@ -19,41 +58,4 @@ function userForm(formProps) {
             });
         }
     };
-}
-
-export default function RegisterVideo() {
-    const [isModalShow, setModalShow] = useState(false);
-    const registerForm = userForm({
-        initialValues: {title: '', url: ''}
-    });
-
-    return (
-        <StyledRegisterVideo>
-            <button className="add-video" onClick={() => setModalShow(true)}>+</button>
-            {
-                isModalShow && (
-                    <form onSubmit={(event) => {
-                        event.preventDefault();
-                        setModalShow(false);
-                        registerForm.clearForm();
-                        console.log(registerForm.values)
-                    }}>
-                        <div>
-                            <button type="button" className="close-modal" onClick={() => setModalShow(false)}>x</button>
-
-                            <input type="text" placeholder="Video title" value={registerForm.values.title}
-                                   name="title"
-                                   onChange={registerForm.handleChange}/>
-
-                            <input type="text" placeholder="Url" value={registerForm.values.url}
-                                   name="url"
-                                   onChange={registerForm.handleChange}/>
-
-                            <button type="submit">Register</button>
-                        </div>
-                    </form>
-                )
-            }
-        </StyledRegisterVideo>
-    )
 }
